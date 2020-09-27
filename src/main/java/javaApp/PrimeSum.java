34704470873a2805ca367d5dc8a7df77d4ca7290
sunan <sunan@DESKTOP-DSB77E9>
2020-09-28 01:54:13 +0530

    Initial

--- src/main/java/javaApp/PrimeSum.java
+++ src/main/java/javaApp/PrimeSum.java
@@ -9,8 +9,33 @@ public class PrimeSum {
     static long primesum(int maxNum) {
         int sum = 0, num;
         //insert your code here
-
+        if(maxNum<=0)
+        {
+        	return sum;
+        }
+        for(int number = 1; number <= maxNum; number++)
+        {
+        	boolean isPrime = checkPrime(number); 
+            if (isPrime) { 
+      
+                // Sum the prime number 
+                sum = sum + number; 
+            } 
+        }
+        
 
         return sum;
     }
+    static boolean checkPrime(int numberToCheck) 
+    { 
+        if(numberToCheck == 1) { 
+            return false; 
+        } 
+        for (int i = 2; i*i <= numberToCheck; i++) { 
+            if (numberToCheck % i == 0) { 
+                return false; 
+            } 
+        } 
+        return true; 
+    } 
 } 
