9c45fab54d0dce953483fa8244775b62a58b5273
Git Server Admin <admin@git-server>
2020-09-27 20:07:57 +0000

    Add initial repository

---
+++ src/main/java/javaApp/PrimeSum.java
@@ -0,0 +1,16 @@
+package javaApp;
+
+public class PrimeSum {
+
+    public static void main(String[] args) {
+        System.out.println(primesum(20));
+    }
+
+    static long primesum(int maxNum) {
+        int sum = 0, num;
+        //insert your code here
+
+
+        return sum;
+    }
+} 
